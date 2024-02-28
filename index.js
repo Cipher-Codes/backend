71c901014ee9849977414adf245e5f0aedc13b8a
Cipher Codes <84612418+Cipher-Codes@users.noreply.github.com>
2024-02-28 21:11:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if(parsedUrl.pathname == '/setdata'){
     fs.appendFile(dataUrl, JSON.stringify(queryParameters), 'utf-8', (err) => {
         console.log(err)
     })
-    res.writeHead(200, {'Content-Type': 'application/json', 'access-control-allow-origin' : '*', "access-control-allow-headers" : 'authorized'}); // http header
+    res.writeHead(200, {'Content-Type': 'application/json', 'access-control-allow-origin' : '*', "access-control-allow-headers" : '*'}); // http header
     res.write("Success"); 
     console.log(typeof(JSON.stringify(queryParameters)))
     res.end();
@@ -25,7 +25,7 @@ if(parsedUrl.pathname == '/setdata'){
 else if(parsedUrl.pathname == '/getdata'){
     fs.readFile(dataUrl, 'utf-8', (err, data) => {
         if(!err){
-            res.writeHead(200, {'Content-Type': 'application/json','access-control-allow-origin' : '*', "access-control-allow-headers" : 'authorized'}); // http header
+            res.writeHead(200, {'Content-Type': 'application/json','access-control-allow-origin' : '*', "access-control-allow-headers" : '*'}); // http header
             res.write(data); 
             res.end();
         }else{
